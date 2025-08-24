5b384bf6e2c0ddb6507a7531b4f77407286b840b
PMogu <Chengze042224@163.com>
2025-08-24 15:15:14 +1000

    Add files via upload

--- day1 bubble sort/bubble_sort.py
+++ day1 bubble sort/bubble_sort.py
@@ -1,6 +1,11 @@
+arr = list(map(int, input().split()))
+
 def bubbleSort(arr):
     for i in range(1, len(arr)):
         for j in range(0, len(arr) - i):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
+
+result = bubbleSort(arr)
+print(*result)
